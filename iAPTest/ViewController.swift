b6e453e92cce0a7d68d093d51ead50b32a808ef4
Nithin Gaddam <ngaddam@logitech.com>
2019-04-03 15:49:43 -0700

    Bridge header addded

--- iAPTest/ViewController.swift
+++ iAPTest/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sendButton: UIButton!
+    
+    @IBOutlet weak var bluetoothDeviceStatusLabel: UILabel!
+    @IBOutlet weak var searchDeviceButton: UIButton!
+    @IBOutlet weak var iapStatusLabel: UILabel!
+    @IBOutlet weak var bytesToSend: UITextField!
+    @IBOutlet weak var recievedBytesLabel: UILabel!
+    
+//    var deviceManger: U
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchDeviceAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func sendButtonAction(_ sender: Any) {
+    }
 }
 
---
+++ iAPTest/iAPTest-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  iAPTest-Bridging-Header.h
+//  iAPTest
+//
+//  Created by Nithin Gaddam on 4/3/19.
+//  Copyright © 2019 Logitech. All rights reserved.
+//
+
+#ifndef iAPTest_bridging_header_h
+#define iAPTest_bridging_header_h
+
+#import "EASessionManager.h"
+#import "UEDevice.h"
+
+
+#endif /* iAPTest_bridging_header_h */
